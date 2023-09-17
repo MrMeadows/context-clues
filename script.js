7a8a555d4fccdcd67bc344202d0d805958ead256
Sebastian Meadows <snhmeadows@outlook.com>
2023-09-17 16:16:13 -0500

    adding alertMSG function via 'this.id'

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-    
+    for (var i = 1; i < 101; i++) {
+        var accusation = ('<h3 id="' + i + '">Accusation ' + i + '<h3>')
+        $('body').append(accusation)
+        $('#'+i).on('click', alertMSG)
+    }
+
+    function alertMSG() {
+        alert('Accusation ' + String(this.id) + ': I accuse ' + 'friend' + ', with the ' + 'weapon' + ' in the ' + 'location')
+    }
+
+
+    /*function alertMsg(friend, weapon, location) {
+
+        String(this.id) LOOK HERE AHHH
+        I think this is a better solution than what Matt Morgan suggested in Gravity
+
+    }*/
+
+    //friends[i],weapons[i],locations[i]
+
 })
