7b0d556d709feb23b0e466c0eae75071b28c4f09
Sebastian Meadows <snhmeadows@outlook.com>
2023-09-17 22:20:02 -0500

    adding id to h3 and completing project

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
+
+    var friends = ['Reiner', 'Bertholdt', 'Annie', 'Zeke', 'Pieck']
+    var locations = ['Shiganshina', 'Trost', 'Stohess', 'Ragako', 'the Utgard Castle', 'Orvud', 'the Underground', 'Liberio', 'Ehrmich', 'the Reiss Chapel']
+    var weapons = ['ODM gear', 'AP ODM gear', 'a signal flare', 'a Thunder Spear', 'an Attack Titan punch', 'a Warhammer Titan bolt', 'an Armored Titan stomp', 'a Colossal Titan explosion', 'a Jaw Titan chomp', 'a Female Titan kick', 'a Founder Titan scream', 'a Cart Titan cannon', 'a Beast Titan rock blast', 'a rifle', 'the Flying Boat', 'a horse', 'a potato', 'pure titans', 'a knife', 'a titan injection']
+
     for (var i = 1; i < 101; i++) {
+        accuse(i);
+    }    
+
+    function accuse (i) {
+            
+        var friend = friends[(i % friends.length)];
+        var weapon = weapons[(i % weapons.length)];
+        var location = locations[(i % locations.length)];
+        var accusationAlert = 'Accusation ' + i + ': I accuse ' + friend + ', with ' + weapon + ' in ' + location + '!';
         var accusation = ('<h3 id="' + i + '">Accusation ' + i + '<h3>')
+        
         $('body').append(accusation)
-        $('#'+i).on('click', alertMSG)
-    }
 
-    function alertMSG() {
-        alert('Accusation ' + String(this.id) + ': I accuse ' + 'friend' + ', with the ' + 'weapon' + ' in the ' + 'location')
-    }
+        $('#' + i).on('click', function(){
+            alert(accusationAlert);
+        });
 
-
-    /*function alertMsg(friend, weapon, location) {
-
-        String(this.id) LOOK HERE AHHH
-        I think this is a better solution than what Matt Morgan suggested in Gravity
-
-    }*/
-
-    //friends[i],weapons[i],locations[i]
+    };
 
 })
